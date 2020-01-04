36bfb0cb17bc1f68dc61e6ad3cbf0448991368e3
CAOSExabyte <55938181+CAOSExabyte@users.noreply.github.com>
2020-01-03 16:50:13 -0800

    worked on encoders for scissor lift

--- SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScissorLift.java
+++ SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScissorLift.java
@@ -12,7 +12,9 @@ public class ScissorLift {
     private DcMotor ScissorLiftMotorLeft, ScissorLiftMotorRight;
     int initial = 560; //This is the amount that we need to raise the scissor lift to compensate for the foundation height
     int block = 1120; //This is the amount that the scissor lift must rise for 1 block
-    int position = 0;
+    int pos = 0;
+    boolean pressed = false;
+    int target = 0;
 
 
         public ScissorLift(DcMotor ScissorLiftL, DcMotor ScissorLiftR){
@@ -28,6 +30,65 @@ public class ScissorLift {
             ScissorLiftMotorRight.setPower(ScissorLiftPower);
 
         }
+        public  void LiftControlTest(double y2, boolean left, boolean right, boolean up, boolean down, boolean b2){
+            double ScissorLiftPower = Range.clip( (-y2), -1.0, 1.0);
+            if(y2 != 0 && !pressed) {
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                ScissorLiftMotorLeft.setPower(ScissorLiftPower);
+                ScissorLiftMotorRight.setPower(ScissorLiftPower);
+            }
+            if(!left && !right && !up && !down){
+                pressed = false;
+            }
+            if(up && !pressed){
+                pressed = true;
+                pos += 1;
+            }
+            if(down && !pressed && pos > 0){
+                pressed = true;
+                pos -= 1;
+            }
+            if(right && !pressed){
+                pressed = true;
+                target = ((pos * block) + initial);
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                ScissorLiftMotorLeft.setTargetPosition(target);
+                ScissorLiftMotorRight.setTargetPosition(target);
+                ScissorLiftMotorLeft.setPower(0.6);
+                ScissorLiftMotorRight.setPower(0.6);
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(ScissorLiftMotorLeft.isBusy() || ScissorLiftMotorRight.isBusy()){}
+                ScissorLiftMotorLeft.setPower(0);
+                ScissorLiftMotorRight.setPower(0);
+
+            }
+            if(left && !pressed){
+                pressed = true;
+                target = ((pos * block) + initial);
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                ScissorLiftMotorLeft.setTargetPosition(-target);
+                ScissorLiftMotorRight.setTargetPosition(-target);
+                ScissorLiftMotorLeft.setPower(-0.6);
+                ScissorLiftMotorRight.setPower(-0.6);
+                ScissorLiftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                ScissorLiftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(ScissorLiftMotorLeft.isBusy() || ScissorLiftMotorRight.isBusy()){}
+                ScissorLiftMotorLeft.setPower(0);
+                ScissorLiftMotorRight.setPower(0);
+
+            }
+            if(b2 && !pressed){
+
+            }
+    }
 /*        public void LiftMovement(boolean scissorUp, boolean scissorDown, boolean reset, boolean activate){
             if(scissorUp){ position += 1; }
             if(scissorDown && position > 0){position -= 1;}
