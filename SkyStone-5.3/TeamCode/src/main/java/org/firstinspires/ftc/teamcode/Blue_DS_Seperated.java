96837759754753f2774d063320184b52fa084547
CAOSExabyte <55938181+CAOSExabyte@users.noreply.github.com>
2020-01-13 12:47:11 -0800

    .

--- SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue_DS_Seperated.java
+++ SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue_DS_Seperated.java
@@ -56,7 +56,7 @@ public class Blue_DS_Seperated extends LinearOpMode {
     double P1SS2ToFoundation = 2.45;
     double P2SS2ToFoundation = 2.65;
     double P3SS2ToFoundation = 2.45;
-    double Foundation2Skybride = 0.7;
+    double Foundation2Skybride = 0.5;
 
     private double r_time = 3.5;
     private double numTime = 0.5;
@@ -236,7 +236,7 @@ public class Blue_DS_Seperated extends LinearOpMode {
                 intake_systems.intake(false,false);
 
                 // Rotating to face the building site
-                Gyro.rotate(107,0.5);
+                Gyro.rotate(105,0.5);
                 sleep(150);
 
                 // Driving to the building site with the second skystone
