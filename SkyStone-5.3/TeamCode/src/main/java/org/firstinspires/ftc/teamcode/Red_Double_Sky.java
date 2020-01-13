5ae7da36e57dcfa02958d534e7b9e4f2b8ce68fa
CAOSDellComputer <caosftc@gmail.com>
2020-01-13 13:47:39 -0800

    Added Red DS seperated and updated it in order to test
    Disabled Red Double Sky as well

--- SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Double_Sky.java
+++ SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Double_Sky.java
@@ -27,6 +27,7 @@ import java.util.List;
 
 
 @Autonomous(name= "Red Double Sky", group="Red")
+@Disabled
 public class Red_Double_Sky extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor  FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor, IntakeLeftMotor, IntakeRightMotor;
