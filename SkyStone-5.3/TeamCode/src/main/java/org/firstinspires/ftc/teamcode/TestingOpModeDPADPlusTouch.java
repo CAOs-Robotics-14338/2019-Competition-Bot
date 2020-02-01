f50122eb4cd72892f6d34e5fd392b9615166ccf0
CAOSDellComputer <caosftc@gmail.com>
2020-02-01 12:44:16 -0800

    Wrote Basic Comments for the Main Teleop Opmode

--- SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModeDPADPlusTouch.java
+++ SkyStone-5.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModeDPADPlusTouch.java
@@ -56,18 +56,19 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TestingOpModeDPADPlusTouch extends OpMode
 {
     // Declare OpMode members.
+    // Declare All Variables of Motors and Servos Used
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor  FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor, IntakeLeftMotor, IntakeRightMotor, ScissorLiftMotorLeft, ScissorLiftMotorRight;
     private Servo IntakePulley, left_hook, right_hook , claw , wrist;
     private CRServo expansion;
 
+    //Declare the Subsystems used in this OpMode
     HolonomicDrive holonomicDrive;
     ScissorLift scissorLift;
     Intake_Systems intake_systems;
     BotServos bot_servo;
-
     ArmCollection armCollection;
-
+// Declare the Touch Sensor
     DigitalChannel touch;
 
     /*
@@ -80,6 +81,8 @@ public class TestingOpModeDPADPlusTouch extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
+
+        //Initialize all the Hardware Devices and find their location on the Expansion Hub
         FrontRightMotor  = hardwareMap.get(DcMotor.class, "front_right_drive");
         FrontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_drive");
         BackRightMotor  = hardwareMap.get(DcMotor.class, "back_right_drive");
@@ -88,26 +91,23 @@ public class TestingOpModeDPADPlusTouch extends OpMode
         IntakeRightMotor = hardwareMap.get(DcMotor.class, "right_intake");
         ScissorLiftMotorLeft =  hardwareMap.get(DcMotor.class, "scissor_left");
         ScissorLiftMotorRight =  hardwareMap.get(DcMotor.class, "scissor_right");
+
         IntakePulley = hardwareMap.servo.get("intake_pulley");
         left_hook = hardwareMap.servo.get("left_hook");
         right_hook = hardwareMap.servo.get("right_hook");
-
         claw = hardwareMap.servo.get("claw");
         wrist = hardwareMap.servo.get("wrist");
         expansion = hardwareMap.get(CRServo.class,"expansion");
+
         touch = hardwareMap.get(DigitalChannel.class, "touch");
-        // set the digital channel to input.
-        touch.setMode(DigitalChannel.Mode.INPUT);
+        touch.setMode(DigitalChannel.Mode.INPUT); // set the digital channel to input.
 
-
+//Construct the subsystems using the declared & initialized hardware devices
         holonomicDrive = new HolonomicDrive(FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor);
         scissorLift = new ScissorLift(ScissorLiftMotorLeft, ScissorLiftMotorRight, IntakePulley);
         intake_systems = new Intake_Systems(IntakeRightMotor, IntakeLeftMotor, IntakePulley, touch, claw);
         bot_servo = new BotServos(left_hook, right_hook);
-
         armCollection = new ArmCollection(claw, wrist, expansion, IntakePulley);
-
-
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
     }
@@ -132,30 +132,32 @@ public class TestingOpModeDPADPlusTouch extends OpMode
      */
     @Override
     public void loop() {
+        //Set Up All GameControllers
+
+        //Drive
         double x = gamepad1.left_stick_x;
         double y = -gamepad1.left_stick_y;
         double z = gamepad1.right_stick_x;
-
         double slow = gamepad1.right_trigger;
 
-        double y2 = gamepad2.left_stick_y;
+        //Collector
         boolean collect1 = gamepad1.a;
         boolean deploy1 = gamepad1.b;
-        boolean collect2 = gamepad2.a;
-        boolean deploy2 = gamepad2.b;
-      //  boolean activate = gamepad2.right_bumper;
-      //  boolean reset = gamepad2.left_bumper;
-        boolean foundation_control1 = gamepad1.right_bumper; //foundation movers
-        boolean foundation_control2 = gamepad1.left_bumper;
-      //  boolean release_collection1 = gamepad1.start; //release collection arms
         boolean release_collection2 = gamepad2.a;
-      //  boolean pullback_collection1 = gamepad1.back; // pull back collection arms
         boolean pullback_collection2 = gamepad2.b;
 
-        double expand_control = gamepad2.right_stick_y;
+        //Foundation Servos
+        boolean foundation_control1 = gamepad1.right_bumper; //foundation movers
+        boolean foundation_control2 = gamepad1.left_bumper;
 
+        //Scissor Lift
+        double lift = gamepad2.left_stick_y;
+
+        //Expansion Arm
         boolean dpad_up = gamepad2.dpad_up;
         boolean dpad_down = gamepad2.dpad_down;
+
+        //Claw
         boolean grab_control = gamepad2.x;
         boolean stone_release_control = gamepad2.y;
         double wrist_control = gamepad2.right_stick_x;
@@ -163,26 +165,29 @@ public class TestingOpModeDPADPlusTouch extends OpMode
         boolean fineWristLeft = gamepad2.left_bumper;
         boolean fineWristMiddle = gamepad2.start;
 
+        //Running the subsystems with the controls
 
-
+        //Drive
         holonomicDrive.teleopDrive(x,y,z, slow);
+        //Collection
         intake_systems.inTel(collect1, deploy1);
         intake_systems.pullBackCollectionArms(pullback_collection2);
         intake_systems.releaseCollectionArms(release_collection2);
-        scissorLift.LiftControl(-y2);
+        // Foundation Servos
         bot_servo.retract(foundation_control2);
         bot_servo.activate(foundation_control1);
-        //scissorLift.LiftMovement(scissorUp, scissorDown, reset, activate);
-        // Show the elapsed game time and wheel power.
-
-
-        //armCollection.expandControl(expand_control);
+        //Scissor Lift
+        scissorLift.LiftControl(-lift);
+       //Expansion Arm
         armCollection.expandControlDPAD(dpad_up, dpad_down);
+        //Claw
         armCollection.grab(grab_control);
         armCollection.release(stone_release_control);
         armCollection.wristControl(wrist_control, fineWristRight, fineWristLeft, fineWristMiddle);
 
-
+        //Testing if the Touch Sensor was pressed which will signal a comment saying that the stone has been collected.
+        //This touch sensor is used to see if the stone has been collected when we the robot is on the opposite side of
+        // the field and we can't see whether or not we collected the stone or capstone.
         if (intake_systems.getTouch())
             telemetry.addData("The Stone's been collected", "");
         telemetry.addData("Status", "Run Time: " + runtime.toString());
